890eadbbdb76db603f52a5c21d868e6c0ea214a6
Luc Videau <73294402+GerfautGE@users.noreply.github.com>
2025-02-19 19:55:38 +0100

    feat: copy on write

--- kernel/vm.c
+++ kernel/vm.c
@@ -7,6 +7,9 @@
 #include "fs.h"
 #include "syscrg.h"
 
+// counter of references to each page
+int ref_count[PHYSTOP/PGSIZE];
+
 /*
  * the kernel's page table.
  */
@@ -70,9 +73,6 @@ kvminit(void)
 void
 kvminithart()
 {
-  // wait for any previous writes to the page table memory to finish.
-  sfence_vma();
-
   w_satp(MAKE_SATP(kernel_pagetable));
 
   // Depending on the cpu implementation a memory barrier might
@@ -329,20 +329,20 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
+    // unset write flag and set copy-on-write flag
+    *pte &= ~PTE_W;
+    *pte |= PTE_COW;
     pa = PTE2PA(*pte);
+    ref_count[pa/PGSIZE]++;
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       goto err;
     }
   }
